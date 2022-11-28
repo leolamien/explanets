920440d75bfc7eb07dfd05a1f3ac3ea084d0644c
leolamien <90797794+leolamien@users.noreply.github.com>
2022-11-28 11:55:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-const PgUser = 'postgres';
-const PgHost = 'localhost';
-const PgDb = DATABASE_URL;
-const PgPassword = 'ipl';
-const PgPort = 5432;
 
 module.exports = {
-    ConnectionString: 'postgres://' + PgUser + ':' + PgPassword + '@' + PgHost + ':' + PgPort + '/' + PgDb
+    ConnectionString: DATABASE_URL
 };
